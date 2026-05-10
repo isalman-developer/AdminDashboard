eef28a714e100c95f7fb6296a70a944352caf048
Salman K <salman.khan@codedistrict.com>
2026-05-10 15:40:38 +0500

    chore: remove Vue.js and related dependencies to standardize on server-side rendering

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/test', 'admin.welcome');
 Route::view('/admin/login', 'admin.auth.login');
-/**
- * Catch ALL routes and send to Vue and Vue Router will handle the actual routing
- * Matches every possible URL and Returns the same Blade view every time
- * e.g. /, /about, /dashboard, /users/1, /posts/2020/05/01/some-post-title
- */
-Route::get('/{any}', function () {
-    return view('app'); // Load app.blade.php
-})->where('any', '.*'); 
+Route::view('/', 'admin.welcome');
